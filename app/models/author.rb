1e465041202e26847dc82bf37d435836f2f4f575
Max Gorin <gorinme@gmail.com>
2014-05-30 10:34:08 +0400

    Add relations between book/author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ActiveRecord::Base
+  has_many :books
+
+  def full_name_with_space
+    first_name + " " + last_name
+  end
+
+  def full_name
+    last_name + ", " + first_name
+  end
 end
